96b75735f6e3be12688f45e982c3cc7e8eb33677
Wizo101 <winson.yap2006@gmail.com>
2024-10-12 17:48:47 +0800

    c12

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,6 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-<<<<<<< HEAD
   
   
 })
-=======
-});
->>>>>>> 8ea3563af5e464d2fc49eb3d94c7a1b25f76af39
