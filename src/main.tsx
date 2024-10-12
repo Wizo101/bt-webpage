cb5575182ff2a8902f08737765eca33340ecfca0
Wizo101 <winson.yap2006@gmail.com>
2024-10-12 11:11:17 +0800

    c5

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.scss'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import { HashRouter } from 'react-router-dom';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDOM.render(
+  <React.StrictMode>
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </React.StrictMode>,
+  document.getElementById('app')
+);
